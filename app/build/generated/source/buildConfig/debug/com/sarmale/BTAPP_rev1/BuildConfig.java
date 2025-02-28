5017d638e747f9d546429342e7b0ac2d6b329163
raveenm <raveenm@mcmaster.ca>
2025-02-27 20:30:57 -0500

    test3

---
+++ app/build/generated/source/buildConfig/debug/com/sarmale/BTAPP_rev1/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.sarmale.BTAPP_rev1;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.sarmale.arduinobtexample_v3";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
